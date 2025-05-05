f6f2e828285cf16382d63eb789444f65442a9157
rammsanker00 <sankeramm54@gmail>
2025-05-05 16:31:47 +0530

    Galllery Access This

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
 // Import necessary packages
 import React from 'react';
-import { StyleSheet, View, SafeAreaView } from 'react-native';
+import { StyleSheet, View, SafeAreaView, Alert } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 // Main component
 const App = () => {
+  const onMessage = (event:any) => {
+    const message = event.nativeEvent.data;
+    Alert.alert('Message Received', 'HI');
+    if (message === 'openGallery') {
+      Alert.alert('Message Received', 'HI');
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <WebView
         source={{ uri: 'https://www.academicgyan.com/' }}
         style={styles.webview}
-        scalesPageToFit={true}
         javaScriptEnabled={true}
         domStorageEnabled={true}
+        onMessage={onMessage}
+        injectedJavaScript={`
+          document.addEventListener("message", function(event) {
+            window.ReactNativeWebView.postMessage(event.data);
+          });
+        `}
       />
     </SafeAreaView>
   );
